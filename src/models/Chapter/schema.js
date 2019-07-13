bc480a4bbfb6761a315e044d51517b7573f9636e
HoangTrongMinhDuc <31062901+HoangTrongMinhDuc@users.noreply.github.com>
2019-07-13 22:36:25 +0700

    Add schema (#6)
    * add schema
    
    * add model
    
    * update chapter schema

--- src/models/Chapter/schema.js
+++ src/models/Chapter/schema.js
@@ -5,6 +5,7 @@ const chapterSchema = new Schema(
     book: { type: Schema.Types.ObjectId, required: true },
     title: { type: String, required: true },
     servers: { type: Map },
+    views: {type: Schema.Types.String, default: 0},
     updated_by: { type: Schema.Types.ObjectId },
     created_at: { type: Schema.Types.Date, default: Date.now },
     available: { type: Schema.Types.Boolean, default: true },
