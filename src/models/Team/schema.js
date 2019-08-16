22c9adcda12e3be7d14abe3bad6305f6f3f1f1d3
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-08-16 21:52:17 +0700

    update  schema & validator

--- src/models/Team/schema.js
+++ src/models/Team/schema.js
@@ -2,7 +2,7 @@ const Schema = require("mongoose").Schema;
 
 const teamSchema = new Schema(
   {
-    logo: { type: String, default: "" },
+    logo: { type: String, default: "https://res.cloudinary.com/shieldmanga/image/upload/v1565883771/manga/logo.png" },
     name: { type: String, required: true },
     description: { type: String, default: "None" },
     leader: { type: Schema.Types.ObjectId, required: true },
