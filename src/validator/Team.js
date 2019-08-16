0a6252d0b035e694a517d3d7268ee5f9389a5377
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-08-16 21:52:17 +0700

    update  schema & validator

--- src/validator/Team.js
+++ src/validator/Team.js
@@ -1,11 +1,13 @@
 const Joi = require("@hapi/joi");
 const Schema = require("./Schema");
 
-const schema = Joi.object().keys({
-  name: Schema.StringName.required(),
-  description: Schema.StringDescription,
-  logo: Schema.StringLink
-});
+const schema = Joi.object()
+  .keys({
+    name: Schema.StringName.required(),
+    description: Schema.StringDescription,
+    logo: Schema.StringLink
+  })
+  .unknown(true);
 
 const isValidTeamInfo = team => !schema.validate(team).error;
 
