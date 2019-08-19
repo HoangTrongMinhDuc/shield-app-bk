7836682709a4189016b1c64968137f0fb59028f7
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-08-20 00:20:06 +0700

    add pageSize option

--- src/helpers/QueryHelper.js
+++ src/helpers/QueryHelper.js
@@ -2,13 +2,13 @@ const { DEF_PAGE_SIZE } = require("../config");
 const { PageOverflowMsg } = require("../Messages");
 
 const paginateQuery = async (query, options) => {
-  const { Doc, page = 1 } = options;
+  const { Doc, page = 1, pageSize = DEF_PAGE_SIZE } = options;
   if (!Doc) return { code: 500 };
   const total = await Doc.countDocuments(query).exec();
-  const pages = Math.ceil(total / DEF_PAGE_SIZE);
+  const pages = Math.ceil(total / pageSize);
   if (page > pages) return { code: 400, message: PageOverflowMsg };
   query.sort("_id");
-  query.skip(DEF_PAGE_SIZE * (page - 1)).limit(DEF_PAGE_SIZE);
+  query.skip(pageSize * (page - 1)).limit(pageSize);
   const data = await query.exec();
   return { pages, page, data };
 };
