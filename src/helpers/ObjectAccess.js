ccf04728f6f7e1e0e72f93705a89db5bb6f184be
hoangtrongminhduc <hoangtrongminhduc@hotmail.com>
2019-08-23 16:39:28 +0700

    fix func check value

--- src/helpers/ObjectAccess.js
+++ src/helpers/ObjectAccess.js
@@ -2,7 +2,7 @@ const has = require("has-value");
 const get = require("get-value");
 const { isBoolean } = require("lodash");
 
-const isObjectHasValue = (object, keyPath) => !has(object, keyPath);
+const isObjectHasValue = (object, keyPath) => has(object, keyPath);
 
 const isAllowProperty = (object, keyPath) =>
   isObjectHasValue(object, keyPath) &&
